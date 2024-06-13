2f28b60b7f1039d7b8f0556526d37aed2f85e642
João Lucas Felix de Macena <131909907+Joao-lucas-felix@users.noreply.github.com>
2024-06-13 20:25:28 -0300

    Feat:  Initial File Management Settings and Pixel Art Endpoint Creation

--- API/pixelforge/src/main/java/br/com/pixelforge/configs/SecurityConfig.java
+++ API/pixelforge/src/main/java/br/com/pixelforge/configs/SecurityConfig.java
@@ -62,6 +62,7 @@ public class SecurityConfig {
                                     .requestMatchers("/swagger-ui/**").permitAll()
                                     .requestMatchers("/v3/api-docs/**").permitAll()
                                     .requestMatchers("/auth/login").permitAll()
+                                    .requestMatchers("/api/pixel-art/**").permitAll()
                             ;
 
                         }
---
+++ API/pixelforge/src/main/java/br/com/pixelforge/controllers/PixelArtController.java
@@ -0,0 +1,54 @@
+package br.com.pixelforge.controllers;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.websocket.server.PathParam;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "Pixel arts", description = "Endpoint for maneging Pixel Arts")
+@RestController
+@RequestMapping("/api/pixel-art")
+public class PixelArtController {
+
+    //non-authenticated
+    @GetMapping
+    public ResponseEntity<String> getNewPixelArts(){
+        return ResponseEntity.ok("Get Works!");
+    }
+
+    //non-authenticated
+    @GetMapping("/searchByName")
+    public ResponseEntity<String> getArtByName(
+            @PathParam("name")String name
+    )
+    {
+        return ResponseEntity.ok("Get by name Wokrs! name in path "+name);
+    }
+
+    //authenticated
+    @PostMapping
+    public ResponseEntity<String> savePixelArt(
+            //Request Body with the data and an art file.
+    ){
+        return ResponseEntity.ok("Post works!");
+    }
+
+
+    //authenticated and be the owner
+    @PutMapping
+    public ResponseEntity<String> updatePixelArt(
+        //Request Body with the data and an art file (the file for update is optional).
+    ){
+        return ResponseEntity.ok("Put works!");
+    }
+
+
+    //authenticated and be the owner or admim
+    @DeleteMapping
+    public ResponseEntity<String> deletePixelArt(
+            // Unique Indentifier of a pixel art
+    ){
+        return ResponseEntity.ok("Delete Works");
+    }
+
+}
