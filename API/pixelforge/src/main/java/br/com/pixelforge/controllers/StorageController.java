7c625a5573f4f8bd91e26503ae56c9c3670f3a5c
João Lucas Felix de Macena <131909907+Joao-lucas-felix@users.noreply.github.com>
2024-07-27 17:51:30 -0300

    Feat: implementation of the file download method in the files storage controller.

--- API/pixelforge/src/main/java/br/com/pixelforge/controllers/StorageController.java
+++ API/pixelforge/src/main/java/br/com/pixelforge/controllers/StorageController.java
@@ -2,13 +2,14 @@ package br.com.pixelforge.controllers;
 
 import br.com.pixelforge.domain.DTOs.UploadArtFileResponseDto;
 import br.com.pixelforge.services.FileStorageService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -36,6 +37,31 @@ public class StorageController {
         return ResponseEntity.ok(new UploadArtFileResponseDto(
                 fileName, fileDownloadUri, file.getContentType(), file.getSize()
         ));
+    }
+    @GetMapping("/downloadFile/{filename:.+}")
+    public ResponseEntity<Resource> downloadFile(
+            @PathVariable String filename, HttpServletRequest request) {
+
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+        logger.info("Reading a file on disk");
+
+        Resource resource = service.loadFileAsResource(filename, userName);
+        String contentType = "";
+
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (Exception e) {
+            logger.info("Could not determine file type!");
+        }
+
+        if (contentType.isBlank()) contentType = "application/octet-stream";
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(
+                        HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 
 }
