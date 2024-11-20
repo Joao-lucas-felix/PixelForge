73444b6429e170643d82298fd429134ebfe97ab5
João Lucas Felix de Macena <131909907+Joao-lucas-felix@users.noreply.github.com>
2024-11-20 16:01:28 -0300

    Refactor: The logic in this endpoint are moving to a PixelArtController and this Controller will be discontinued.

--- API/pixelforge/src/main/java/br/com/pixelforge/controllers/PixelArtController.java
+++ API/pixelforge/src/main/java/br/com/pixelforge/controllers/PixelArtController.java
@@ -1,80 +1,93 @@
 package br.com.pixelforge.controllers;
 
+import br.com.pixelforge.domain.DTOs.FileInfoDto;
 import br.com.pixelforge.domain.DTOs.PixelArtDto;
+import br.com.pixelforge.domain.DTOs.UploadArtFileResponseDto;
+import br.com.pixelforge.services.FileStorageService;
 import br.com.pixelforge.services.PixelArtServices;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.websocket.server.PathParam;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.PagedModel;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-@Tag(name = "Pixel arts", description = "Endpoint for maneging Pixel Arts")
+@Slf4j
 @RestController
 @RequestMapping("/api/pixel-art/v1")
 public class PixelArtController {
-
-    private final PixelArtServices services;
-
     @Autowired
-    public PixelArtController(PixelArtServices services) {
-        this.services = services;
-    }
+    private FileStorageService service;
+    @Autowired
+    private PixelArtServices pixelArtServices;
 
 
-    //non-authenticated
-    @GetMapping("/searchByName")
-    public ResponseEntity<String> getArtByName(
-            @PathParam("name")String name
-    )
-    {
-        return ResponseEntity.ok("Get by name Wokrs! name in path "+name);
-    }
-
-    @GetMapping
-    public ResponseEntity<PagedModel<EntityModel<PixelArtDto>>>
-    findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "size", defaultValue = "6") Integer size,
-            @RequestParam(value = "direction", defaultValue = "asc") String direction) {
-        var sort = "desc".equalsIgnoreCase(direction) ? Sort.Direction.DESC : Sort.Direction.ASC;
-        Pageable pageable = PageRequest.of(page, size, Sort.by(sort, "name"));
-        return ResponseEntity.ok(services.findAllPixelArts(pageable));
-    }
-
-
-    //authenticated
     @PostMapping
-    public ResponseEntity<PixelArtDto> savePixelArt(
-            @RequestBody PixelArtDto dto
-            )
-    {
-        PixelArtDto pixelArte = this
-                .services.createPixelArt(dto);
-
-        return ResponseEntity.ok(pixelArte);
-    }
-
-
-    //authenticated and be the owner
-    @PutMapping
-    public ResponseEntity<String> updatePixelArt(
-        //Request Body with the data and an art file (the file for update is optional).
+    public ResponseEntity<UploadArtFileResponseDto> createPixelArt(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("name") String name,
+            @RequestParam("description") String description,
+            @RequestParam("isFreeUse") Boolean isFreeUse
     ){
-        return ResponseEntity.ok("Put works!");
+        log.info("Create an Pixel Art");
+
+        //Getting the user with the security context
+        var userName = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        log.info("Storing the file in disk.");
+
+        String fileName = service.storageFile(file,userName );
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("api/file/v1/download/"+fileName).toUriString();
+
+        var fileInfoDto = new FileInfoDto(fileName,
+                fileDownloadUri, file.getContentType(), file.getSize());
+
+        log.info("Storing the pixel art info in the DataBase");
+        log.info("Name {}", name);
+        log.info("Description {}", description);
+        log.info("Is Free Use {}", isFreeUse);
+
+        var createDto = new PixelArtDto(null,  name, description,
+                file.getOriginalFilename(), isFreeUse, userName);
+        var pixelArtResponseDto = pixelArtServices.createPixelArt(createDto);
+
+        return ResponseEntity.ok(new UploadArtFileResponseDto(
+                 pixelArtResponseDto, fileInfoDto
+        ));
+
     }
 
 
-    //authenticated and be the owner or admim
-    @DeleteMapping
-    public ResponseEntity<String> deletePixelArt(
-            // Unique Indentifier of a pixel art
-    ){
-        return ResponseEntity.ok("Delete Works");
-    }
+    @GetMapping("/downloadFile/{filename:.+}")
+    public ResponseEntity<Resource> downloadFile(
+            @PathVariable String filename, HttpServletRequest request) {
 
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+        log.info("Reading a file on disk");
+
+        Resource resource = service.loadFileAsResource(filename, userName);
+        String contentType = "";
+
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (Exception e) {
+            log.info("Could not determine file type!");
+        }
+
+        if (contentType.isBlank()) contentType = "application/octet-stream";
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(
+                        HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
 
 }
