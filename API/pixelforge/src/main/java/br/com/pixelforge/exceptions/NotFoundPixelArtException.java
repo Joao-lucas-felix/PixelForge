8ae486431700b527529458e1d66b9981101bbe7a
João Lucas Felix de Macena <131909907+Joao-lucas-felix@users.noreply.github.com>
2024-11-21 13:39:13 -0300

    Feat: create the new exception

---
+++ API/pixelforge/src/main/java/br/com/pixelforge/exceptions/NotFoundPixelArtException.java
@@ -0,0 +1,20 @@
+package br.com.pixelforge.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NotFoundPixelArtException
+        extends AuthenticationException
+        implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public NotFoundPixelArtException(String message) {
+        super(message);
+    }
+}
