10a9ca675988f6244dadf41f4c0ab6560ac76add
João Lucas Felix de Macena <131909907+Joao-lucas-felix@users.noreply.github.com>
2024-11-21 13:38:51 -0300

    Refactor: changed the file storage service

--- API/pixelforge/src/main/java/br/com/pixelforge/services/FileStorageService.java
+++ API/pixelforge/src/main/java/br/com/pixelforge/services/FileStorageService.java
@@ -1,7 +1,6 @@
 package br.com.pixelforge.services;
 
 import br.com.pixelforge.configs.FileUploadConfig;
-import br.com.pixelforge.domain.ValidationFilePathInfo;
 import br.com.pixelforge.exceptions.FileStorageException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
@@ -10,7 +9,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
