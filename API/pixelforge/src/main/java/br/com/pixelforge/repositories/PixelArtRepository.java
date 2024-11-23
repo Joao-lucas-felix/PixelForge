22c1b94e150c812b01704f9d9a6acb750cb3c171
João Lucas Felix de Macena <131909907+Joao-lucas-felix@users.noreply.github.com>
2024-11-23 15:49:41 -0300

    Feat: Implementation of the search by name method

--- API/pixelforge/src/main/java/br/com/pixelforge/repositories/PixelArtRepository.java
+++ API/pixelforge/src/main/java/br/com/pixelforge/repositories/PixelArtRepository.java
@@ -1,7 +1,14 @@
 package br.com.pixelforge.repositories;
 
 import br.com.pixelforge.domain.PixelArt;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PixelArtRepository extends JpaRepository<PixelArt, Long> {
+    @Query("SELECT p FROM PixelArt p WHERE p.name LIKE LOWER(CONCAT ('%',:name,'%'))")
+    Page<PixelArt> findByName(String name, Pageable pageable);
 }
